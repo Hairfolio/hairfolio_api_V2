e060782f542993c7ef74e94da460e86c4f8a2d0b
Tyler Horan <tylerhoran@gmail.com>
2016-12-08 15:58:05 -0800

    ading password protect admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,11 @@
 RailsAdmin.config do |config|
 
+  config.authorize_with do
+    authenticate_or_request_with_http_basic('Site Message') do |username, password|
+      username == 'admin@example.com' && password == 'password'
+    end
+  end
+
   ### Popular gems integration
 
   ## == Devise ==
