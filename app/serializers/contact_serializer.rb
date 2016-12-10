f755026e1b60015f23b308b42c73eb63cb819f55
Tyler Horan <tylerhoran@gmail.com>
2016-12-09 16:09:09 -0800

    adding docs for the contacts

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,5 +1,5 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :first_name, :last_name, :company
+  attributes :id, :created_at, :first_name, :last_name, :company, :address
   #has_many :posts
   has_many :emails
   has_many :phones
