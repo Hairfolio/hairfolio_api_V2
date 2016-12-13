05ac184e64434e75057e6290a5890c5c6e083c7e
Tyler Horan <tylerhoran@gmail.com>
2016-12-12 17:57:21 -0800

    adding specs

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,6 +1,6 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :first_name, :last_name, :company, :address
-  #has_many :posts
+  attributes :id, :created_at, :first_name, :last_name, :company, :address, :city, :state, :zipcode
+  has_many :posts
   has_many :emails
   has_many :phones
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161213014727) do
+ActiveRecord::Schema.define(version: 20161213015051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,10 @@ ActiveRecord::Schema.define(version: 20161213014727) do
     t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "city"
+    t.string   "state"
+    t.string   "zipcode"
+    t.string   "photo_url"
     t.index ["user_id"], name: "index_contacts_on_user_id", using: :btree
   end
 
