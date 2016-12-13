0ead30dd7d7bf10d3e6cca116a1f013eff593b6e
Tyler Horan <tylerhoran@gmail.com>
2016-12-13 07:47:32 -0800

    adding followers count

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,16 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :auth_token, :email, :first_name, :last_name, :auth_token, :account_type, :is_following, :salon, :avatar_url, :avatar_cloudinary_id, :share_facebook, :share_twitter, :share_instagram, :share_pinterest, :share_tumblr, :prof_desc, :years_exp, :career_opportunity
+  attributes :id, :auth_token, :email, :first_name, :last_name, :auth_token, :likes_count, :followers_count, :account_type, :is_following, :salon, :avatar_url, :avatar_cloudinary_id, :share_facebook, :share_twitter, :share_instagram, :share_pinterest, :share_tumblr, :prof_desc, :years_exp, :career_opportunity
   has_many :likes
   has_many :educations
   has_many :offerings
+
+  def likes_count
+    object.likes.length
+  end
+
+  def followers_count
+    object.followers.length
+  end
 
   def salon
     SalonSerializer.new(object.salon).serializable_hash if object.salon
