7d22e83db30f41905ae44ee5d34eddebcc00e837
Tyler Horan <tylerhoran@gmail.com>
2016-12-15 11:30:04 -0800

    adidng docs

--- app/serializers/conversation_serializer.rb
+++ app/serializers/conversation_serializer.rb
@@ -1,12 +1,12 @@
 class ConversationSerializer < ActiveModel::Serializer
-  attributes :id, :sender_id, :created_at, :last_message, :recipient_ids, :unread_count
+  attributes :id, :sender_id, :created_at, :last_message, :recipient_ids, :unread_messages
 
   def last_message
     MessageSerializer.new(object.messages.last, {scope: scope}).serializable_hash if object.messages.any?
   end
 
-  def unread_count
-    scope && scope.current_user ? object.messages.where(read: false).where('user_id != ?', scope.current_user.id).length : 0
+  def unread_messages
+    scope && scope.current_user ? (object.messages.where(read: false).where('user_id != ?', scope.current_user.id).length > 0) : 0
   end
 
   def recipient_ids
