62337e440ad31350e2c9af6283f395b8e1c07926
Tyler Horan <tylerhoran@gmail.com>
2016-12-15 08:52:05 -0800

    adding user back to post

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,6 +1,11 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :description, :likes_count, :comments_count
+  attributes :id, :created_at, :description, :likes_count, :comments_count, :liked_by_me
   has_many :photos
   has_many :videos
+  has_one :user
+
+  def liked_by_me
+    object.user.likes.include?(object)
+  end
 
 end
