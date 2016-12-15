0cde85e2ae06f792656b746e63d7d671677826e6
Tyler Horan <tylerhoran@gmail.com>
2016-12-15 12:08:03 -0800

    adding last photo

--- app/serializers/tag_serializer.rb
+++ app/serializers/tag_serializer.rb
@@ -1,3 +1,7 @@
 class TagSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :name, :url
+  attributes :id, :created_at, :name, :url, :last_photo
+
+  def last_photo
+    object.labels.first.photo.asset_url if (object.labels.any? && object.labels.first.photo)
+  end
 end
