17a08621f6b25922695b704e6369264838c86fd7
Tyler Horan <tylerhoran@gmail.com>
2016-12-15 10:36:15 -0800

    adding brands

--- app/controllers/api/v1/brands_controller.rb
+++ app/controllers/api/v1/brands_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::BrandsController < ApplicationController
 
   def index
     brands = Brand.all.page(params[:page]).per(20)
+    brands = Service.find(params[:service_id]).brands if params["service_id"]
+    brands.page(params[:page]).per(20)
     render json: brands, meta: pagination_dict(brands)
   end
 
