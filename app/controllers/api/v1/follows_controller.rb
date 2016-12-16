c869e7470cc1e956b03256d7ea3d2dd85c551f12
Tyler Horan <tylerhoran@gmail.com>
2016-12-16 10:16:51 -0800

    adding follwers

--- app/controllers/api/v1/follows_controller.rb
+++ app/controllers/api/v1/follows_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::FollowsController < ApplicationController
   def create
     follow = Follow.new(follower: current_user, following: @user)
     if follow.save
-      render json: current_user.following, status: 201
+      render json: current_user.following.uniq, status: 201
     else
       render json: { errors: follow.errors }, status: 422
     end
@@ -19,11 +19,11 @@ class Api::V1::FollowsController < ApplicationController
 
   def index
     if params[:followings]
-      users = @user.following.page(params[:page]).per(20)
+      users = @user.following.uniq.page(params[:page]).per(20)
     elsif params[:friends]
-      users = Kaminari.paginate_array(@user.friends).page(params[:page]).per(20)
+      users = Kaminari.paginate_array(@user.friends.uniq).page(params[:page]).per(20)
     else
-      users = @user.followers.page(params[:page]).per(20)
+      users = @user.followers.uniq.page(params[:page]).per(20)
     end
     render json: users, meta: pagination_dict(users)
   end
