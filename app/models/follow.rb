335e2dbe7fdcf3d75a954d8837acd83673bf14ae
Tyler Horan <tylerhoran@gmail.com>
2016-12-16 09:47:55 -0800

    adding fix for follwoing

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,7 +2,6 @@ class Follow < ApplicationRecord
   belongs_to :follower, foreign_key: 'follower_id', class_name: 'User'
   belongs_to :following, foreign_key: 'following_id', class_name: 'User'
   validates_presence_of :follower, :following
-  validates_uniqueness_of :follower_id, scope: :following_id
   after_create :notify_following
 
   def notify_following
