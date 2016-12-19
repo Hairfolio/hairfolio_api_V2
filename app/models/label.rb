d48c5800dab6cef5371e3b488e0f5f2419e82fad
Tyler Horan <tylerhoran@gmail.com>
2016-12-19 14:48:01 -0800

    adding tags

--- app/models/label.rb
+++ app/models/label.rb
@@ -3,7 +3,7 @@ class Label < ApplicationRecord
   belongs_to :tag, counter_cache: true
   has_many :formulas, dependent: :destroy
   accepts_nested_attributes_for :formulas, allow_destroy: true
-  validates_presence_of :tag, :position_top, :position_left
+  validates_presence_of :position_top, :position_left
 
   enum label_type: [:hash_tag, :url]
 end
