102d407123c09a416fb0561efa32d722ad1a6b03
Tyler Horan <tylerhoran@gmail.com>
2016-12-19 10:52:35 -0800

    adding description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219182124) do
+ActiveRecord::Schema.define(version: 20161219185055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -401,6 +401,7 @@ ActiveRecord::Schema.define(version: 20161219182124) do
     t.text     "career_opportunity"
     t.string   "auth_token"
     t.integer  "brand_id"
+    t.text     "description"
     t.index ["brand_id"], name: "index_users_on_brand_id", using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
