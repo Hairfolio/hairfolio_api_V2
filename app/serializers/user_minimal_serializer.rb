9878e75e5765a41f11d3d46402d0285ba0f8d666
Tyler Horan <tylerhoran@gmail.com>
2016-12-23 09:40:39 -0800

    adding

--- app/serializers/user_minimal_serializer.rb
+++ app/serializers/user_minimal_serializer.rb
@@ -1,5 +1,5 @@
 class UserMinimalSerializer < ActiveModel::Serializer
-  attributes :id, :avatar_url, :avatar_cloudinary_id, :first_name, :last_name, :brand_name, :salon_name
+  attributes :id, :avatar_url, :avatar_cloudinary_id, :first_name, :last_name, :brand_name, :salon_name, :account_type
 
   def brand_name
     object.brand ? object.brand.name : nil
