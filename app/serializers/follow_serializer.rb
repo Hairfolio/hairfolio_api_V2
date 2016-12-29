09a3f846c7cad5bd72d2b8e999c91004af1367a5
Tyler Horan <tylerhoran@gmail.com>
2016-12-29 15:09:15 -0800

    adding follow

--- app/serializers/follow_serializer.rb
+++ app/serializers/follow_serializer.rb
@@ -2,10 +2,10 @@ class FollowSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :follower, :following
 
   def follower
-    UserMinimalSerializer.new(object.follower, {scope: scope}).serializable_hash
+    UserMinimalSerializer.new(object.follower, {scope: scope}).serializable_hash if object.follower
   end
 
   def following
-    UserMinimalSerializer.new(object.following, {scope: scope}).serializable_hash
+    UserMinimalSerializer.new(object.following, {scope: scope}).serializable_hash if object.following
   end
 end
