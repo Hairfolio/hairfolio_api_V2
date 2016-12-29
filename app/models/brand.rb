7d7403468b8f3426d83bb1bdc55bfde4f14669d4
Tyler Horan <tylerhoran@gmail.com>
2016-12-29 15:13:55 -0800

    adding admin fixes

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,5 +1,5 @@
 class Brand < ApplicationRecord
   validates_presence_of :name
   has_many :users
-  has_and_belongs_to_many :services, -> { distinct }
+  has_and_belongs_to_many :services
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,4 @@
 class Service < ApplicationRecord
   validates_presence_of :name
-  has_and_belongs_to_many :brands, -> { distinct }
+  has_and_belongs_to_many :brands
 end
