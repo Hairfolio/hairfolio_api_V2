5756b00efa1ccdf036d5e91d1ae920f635dc2c2b
Tyler Horan <tylerhoran@gmail.com>
2017-01-10 16:12:13 -0800

    adding video url

--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -1,5 +1,5 @@
 class PhotoSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :asset_url, :post_id, :labels
+  attributes :id, :created_at, :asset_url, :post_id, :labels, :video_url
 
   def labels
     object.labels.map {|f| LabelSerializer.new(f, {scope: scope}).serializable_hash}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161220194801) do
+ActiveRecord::Schema.define(version: 20170111001104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -260,6 +260,8 @@ ActiveRecord::Schema.define(version: 20161220194801) do
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.string   "url"
+    t.string   "photo_asset_url"
+    t.string   "video_asset_url"
     t.index ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
     t.index ["post_id"], name: "index_messages_on_post_id", using: :btree
     t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
@@ -302,6 +304,7 @@ ActiveRecord::Schema.define(version: 20161220194801) do
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "video_url"
     t.index ["post_id"], name: "index_photos_on_post_id", using: :btree
   end
 
