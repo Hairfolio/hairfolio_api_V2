bb8444ee7cb838b83007fa968e6997d984edb824
Tyler Horan <tylerhoran@gmail.com>
2017-01-23 11:25:37 -0800

    adding code

--- app/serializers/harmony_serializer.rb
+++ app/serializers/harmony_serializer.rb
@@ -1,8 +1,11 @@
 class HarmonySerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :name, :line, :position
-  has_many :colors
+  attributes :id, :created_at, :name, :line, :position, :colors
 
   def line
     LineSerializer.new(object.line, {scope: scope}).serializable_hash
+  end
+
+  def colors
+    object.colors.order('code desc').map { |c| ColorSerializer.new(c).serializable_hash}
   end
 end
