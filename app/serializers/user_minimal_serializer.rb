e1d221d2def950fc9800ed8e67cdd1fb5e10adc2
Tyler Horan <tylerhoran@gmail.com>
2017-02-06 08:02:52 -0800

    adding likes

--- app/serializers/user_minimal_serializer.rb
+++ app/serializers/user_minimal_serializer.rb
@@ -1,5 +1,5 @@
 class UserMinimalSerializer < ActiveModel::Serializer
-  attributes :id, :avatar_url, :avatar_cloudinary_id, :first_name, :last_name, :brand_name, :salon_name, :account_type
+  attributes :id, :avatar_url, :avatar_cloudinary_id, :first_name, :last_name, :brand_name, :salon_name, :account_type, :is_following_me, :is_followed_by_me 
 
   def brand_name
     object.brand ? object.brand.name : nil
@@ -7,5 +7,13 @@ class UserMinimalSerializer < ActiveModel::Serializer
 
   def salon_name
     object.salon ? object.salon.name : nil
+  end
+
+  def is_following_me
+    scope && scope.current_user && object.following?(scope.current_user)
+  end
+
+  def is_followed_by_me
+    scope && scope.current_user && object.followers?(scope.current_user)
   end
 end
