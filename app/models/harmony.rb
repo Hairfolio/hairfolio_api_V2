cde815434f60e14b8c185efc01816c0e5f4f4c09
Tyler Horan <tylerhoran@gmail.com>
2017-02-06 09:08:03 -0800

    adding saloms

--- app/models/harmony.rb
+++ app/models/harmony.rb
@@ -4,6 +4,14 @@ class Harmony < ApplicationRecord
   has_many :colors
 
   def ordered_colors
+    hash = colors.map { |c|
+      {
+        id: c.id,
+        code: c.code,
+        first_digit_or_number: c.code.split(/^([a-zA-Z]{1,}|\d{1,}\.\d{1,}|\d{1,})/).first,
+        last_digit_or_number: c.code.split(/([a-zA-Z]{1,}|\d{1,}\.\d{1,}|\d{1,})$/).first
+      }
+    }
     
   end
 end
