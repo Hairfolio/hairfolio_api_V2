06f8c117011a5f3acf48de31f8a02a92dba7dd32
Tyler Horan <tylerhoran@gmail.com>
2017-02-21 20:24:19 -0800

    adding cloudinary url

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :name, :link_url, :image_url
+  attributes :id, :created_at, :name, :link_url, :image_url, :cloudinary_url
   belongs_to :tag
 
 end
