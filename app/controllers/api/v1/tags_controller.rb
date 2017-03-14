1a9e941a0e4870ca59bbefa5fffe1b39f5458639
Tyler Horan <tylerhoran@gmail.com>
2017-03-14 15:30:20 -0700

    addoing

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -19,7 +19,7 @@ class Api::V1::TagsController < ApplicationController
   end
 
   def posts
-    posts = Post.where(id: @tag.photos.pluck(:post_id)).order('created_at desc').page(params[:page]).per(20)
+    posts = Post.where(id: @tag.photos.pluck(:post_id)).order('created_at desc').page(params[:page]).per(8)
     render json: posts, meta: pagination_dict(posts)
   end
 
