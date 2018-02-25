3943c389ea99259352c4a62abd42e3151908cbe6
Tyler Horan <tylerhoran@gmail.com>
2018-02-25 08:43:59 -0800

    adding cache warming

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,12 +5,22 @@ class Photo < ApplicationRecord
   accepts_nested_attributes_for :labels, allow_destroy: true
   validates_presence_of :asset_url
   after_create :update_last_photo
+  after_create :warm_cache
 
   private
 
   def update_last_photo
     tags.each do |tag|
       tag.update(last_photo: asset_url)
+    end
+  end
+
+  def warm_cache
+    split = asset_url.split('upload')
+    let splitUrl = uri.split('upload');
+    [80, 120, 32, 40, 195, 220].each do |width|
+      new_url = "#{split[0]}upload/w_#{width}#{split[1]}"
+      open(new_url)
     end
   end
 end
--- db/migrate/20180220221713_add_last_photo_to_tag.rb
+++ db/migrate/20180220221713_add_last_photo_to_tag.rb
@@ -1,9 +1,9 @@
 class AddLastPhotoToTag < ActiveRecord::Migration[5.2]
   def change
     add_column :tags, :last_photo, :string
-    Photo.order('created_at asc') do |photo|
+    Photo.order('created_at asc').each do |photo|
       photo.tags.each do |tag|
-        tag.update(last_photo: tag.asset_url)
+        tag.update(last_photo: photo.asset_url)
       end
     end
   end
