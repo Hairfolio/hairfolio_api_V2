c805c1161460920435e29053058ea30826b6cc6e
Tyler Horan <tylerhoran@gmail.com>
2018-03-01 12:55:57 -0800

    adding api

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,12 +1,16 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :description, :likes_count, :comments_count, :liked_by_me
+  attributes :id, :created_at, :description, :likes_count, :user, :comments_count, :liked_by_me
   has_many :photos, include_nested_associations: true
   has_many :videos, include_nested_associations: true
-  has_one :user
+  #has_one :user
 
   # def photos
   #   object.photos.includes(labels: [:tag, formulas: [:service, treatments: [:color]]])
   # end
+
+  def user
+    UserMinimalSerializer.new(object.user, {scope: scope}).serializable_hash
+  end
 
   def liked_by_me
     object.user.likes.pluck(:post_id).include?(object.id)
