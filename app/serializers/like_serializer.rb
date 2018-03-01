f22041701e1c679447d6a2ffcfaa693b533aac66
Tyler Horan <tylerhoran@gmail.com>
2018-03-01 12:35:37 -0800

    adding post

--- app/serializers/like_serializer.rb
+++ app/serializers/like_serializer.rb
@@ -1,9 +1,10 @@
 class LikeSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :post, :user
+  has_one :post
 
-  def post
-    PostSerializer.new(object.post, {scope: scope}).serializable_hash
-  end
+  # def post
+  #   PostSerializer.new(object.post, {scope: scope}).serializable_hash
+  # end
 
   def user
     UserMinimalSerializer.new(object.user, {scope: scope}).serializable_hash
