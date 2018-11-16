5a118c1fc725222f06efbc8e468e57ad7c7c9329
Prakash Vadher <prakash.vadher@agileinfoways.com>
2018-11-16 15:16:42 +0530

    rack gen

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,11 @@ Bundler.require(*Rails.groups)
 
 module Hairfolio
   class Application < Rails::Application
-    #config.load_defaults 5.2
+    # config.load_defaults 5.2
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-        #cors configuration
+        # cors configuration
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
