4a91bc324eef3821675b535fa08763bbf964d5f5
Prakash Vadher <prakash.vadher@agileinfoways.com>
2018-12-05 15:20:25 +0530

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 require 'api_constraints'
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+  namespace :api, defaults: { format: :json }, constraints: { localhost: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       root to: 'home#index', as: 'api_home'
       resources :brands, only: [:index, :show]
@@ -10,9 +10,9 @@ Rails.application.routes.draw do
         collection { get :exact }
         member { get :posts }
       end
-      resources :categories, only: [:index]
-      resources :certificates, only: [:index, :show, :create]
-      resources :products, only: [:index]
+      resources :categories, only: [:index, :show]
+      resources :certificates, only: [:index, :show]
+      resources :products, only: [:index, :show]
       resources :degrees, only: [:index, :show]
       resources :lines, only: [:index, :show]
       resources :harmonies, only: [:index]
@@ -31,6 +31,12 @@ Rails.application.routes.draw do
       resources :posts do
         resources :comments, only: [:create, :destroy, :index, :update]
         resources :likes, only: [:create, :index] do
+          collection { delete :destroy }
+        end
+      end
+      resources :products do
+        # resources :comments, only: [:create, :destroy, :index, :update]
+        resources :favourites, only: [:create, :index] do
           collection { delete :destroy }
         end
       end
@@ -66,8 +72,9 @@ Rails.application.routes.draw do
         end
       end
     end
-  end
-  post "invite_users",to: 'api/v1/users#invite_users'
+  end  
+  get "/user/likes/:id" => "api/v1/users#user_likes"
+  get "/user/favourites/:id" => "api/v1/users#user_favourites"
   get "/404" => "api/v1/errors#not_found"
   get "/500" => "api/v1/errors#exception"
   devise_for :users
