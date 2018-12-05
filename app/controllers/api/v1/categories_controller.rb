d158012ff0a0279d0499f4c5b24d423c52a80604
Prakash Vadher <prakash.vadher@agileinfoways.com>
2018-12-05 12:47:09 +0530

    Add show function

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -4,5 +4,9 @@ class Api::V1::CategoriesController < ApplicationController
     categories = Category.all.page(params[:page]).per(params[:limit])
     render json: categories, meta: pagination_dict(categories)
   end
+  def show
+  	@category = Category.find(params[:id])
+  	render json: @category
+  end
 
 end
