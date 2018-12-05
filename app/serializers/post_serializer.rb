fd47c8d345c09b8b1292338a6e3fab44c61f71ef
Prakash Vadher <prakash.vadher@agileinfoways.com>
2018-12-05 17:44:45 +0530

    Post Data

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,12 +1,12 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :description, :likes_count, :user, :comments_count, :liked_by_me
+  attributes :id, :created_at, :description, :likes_count, :products, :user, :comments_count, :liked_by_me
   has_many :photos, include_nested_associations: true
   has_many :videos, include_nested_associations: true
-  #has_one :user
+  has_one :user
 
-  # def photos
-  #   object.photos.includes(labels: [:tag, formulas: [:service, treatments: [:color]]])
-  # end
+  def photos
+    object.photos.includes(labels: [:tag, formulas: [:service, treatments: [:color]]])
+  end
 
   def user
     UserMinimalSerializer.new(object.user, {scope: scope}).serializable_hash
