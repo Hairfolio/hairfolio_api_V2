fa6f463016a94c370b057ed19959db1c433c0e80
Prakash Vadher <prakash.vadher@agileinfoways.com>
2018-12-05 15:51:13 +0530

    Add rake core gem

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,15 @@ module Hairfolio
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # Rails 5
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+    
     config.autoload_paths << Rails.root.join('lib')
     config.exceptions_app = self.routes
     config.generators do |g|
