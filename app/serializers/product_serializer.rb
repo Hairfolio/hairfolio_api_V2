739a55adbedb3e129f34a06f0ca7d54eb88771ef
Prakash Vadher <prakash.vadher@agileinfoways.com>
2018-12-05 15:59:28 +0530

    Product Serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,13 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :name, :link_url, :image_url, :cloudinary_url
-  belongs_to :tag
+  attributes :id, :name, :is_favourite, :price, :quantity, :favourites_count, :link_url, :image_url, :cloudinary_url, :created_at, :categories
+  belongs_to :tag 
+
+	def is_favourite
+	   object.favourites.pluck(:user_id).include?(current_user_id)	   
+    end
+
+	def current_user_id
+	    @instance_options[:user_id]
+  	end
 
 end
