ef7b275b46cb4d0d02e7e641bee0f77f0a0a7b76
prakash.vadher <prakash.vadher@agileinfoways.com>
2019-02-05 15:30:55 +0530

    favorite product

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -3,11 +3,12 @@ class Favourite < ApplicationRecord
   belongs_to :user
   validates_presence_of :product, :user
   validates_uniqueness_of :product, scope: :user
-  # after_create :notify_owner
-  # has_many :notifications
-
-  # def notify_owner
-  #   Notification.create!(user: post.user, body: "#{user.first_name} #{user.last_name} liked your post.", notifiable: self)
-  # end
+  
+  after_create :notify_owner
+  has_many :notifications
+  # Add new column name as favourite_id in notifications table
+  def notify_owner
+    Notification.create!(user: user, body: "#{product.name} Added to Your Favourite List.", notifiable: self)
+  end
   
 end
