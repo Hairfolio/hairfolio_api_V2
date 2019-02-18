84dc0b17330a5b64a0658136dc831d3cf74aaa02
prakash.vadher <prakash.vadher@agileinfoways.com>
2019-02-18 17:20:29 +0530

    Search & Filter

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,13 +1,21 @@
-class Api::V1::SearchesController < ApplicationController
-    
-    def create   
-        @products = Product.where(nil)
+class Api::V1::SearchesController < ApplicationController    
+
+    def search_products
+    	@products = Product.where(nil)    	
+    	@products = @products.where('lower(name) LIKE ? or lower(description) LIKE ?', "%#{params[:search].downcase}%", "%#{params[:search].downcase}%") if params[:search].present?
+    	@products = @products.page(params[:page]).per(params[:limit]).order('id DESC')
+
+    	render json: @products, meta: pagination_dict(@products)
+    end
+
+    def filter_products
+    	@products = Product.where(nil)
         @products = @products.joins(:categories).where("categories.id IN (?)", params[:categories]) if params[:categories].present?
-        @products = @products.where('lower(products.name) LIKE ? or lower(products.description) LIKE ?', "%#{params[:search].downcase}%", "%#{params[:search].downcase}%").where(:price => params[:price_start]..params[:price_end]) if params[:search].present?
+        @products = @products.where('lower(products.name) LIKE ? or lower(products.description) LIKE ?', "%#{params[:search].downcase}%", "%#{params[:search].downcase}%") if params[:search].present?
         @products = @products.where(:price => params[:price_start]..params[:price_end]) if params[:price_start].present? and params[:price_end].present?
         @products = @products.page(params[:page]).per(params[:limit]).order('id DESC')
         
-        render json: @products.uniq, meta: pagination_dict(@products)
+        render json: @products, meta: pagination_dict(@products)
     end
 
 end
