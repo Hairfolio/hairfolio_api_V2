6773790aacd55922389ba4b90db7a23166e278d7
prakash.vadher <prakash.vadher@agileinfoways.com>
2019-02-28 17:13:12 +0530

    refer save data

--- app/controllers/api/v1/refers_controller.rb
+++ app/controllers/api/v1/refers_controller.rb
@@ -3,14 +3,13 @@ class Api::V1::RefersController < ApplicationController
   	require "base64"
 
 	#Meta tags Api
-  	def view_post_meta
-	    @unique_code = SecureRandom.hex(7)
-	    # if Refer.exists?(user_id: @user_id,post_id: params[:id])
-	    # 	render json: 8888
-	    # else
-	    # 	render json: 0000
-	    # end
-	    # Refer.create(user_id: @user_id, post_id:params[:id], unique_code:@unique_code) if @user_id && params[:id]	    
+  	def view_post_meta	    
+	    if Refer.exists?(user_id: @user_id,post_id: params[:id])
+	    	@unique_code = Refer.where(user: @user_id).where(post_id: params[:id]).map(&:unique_code)[0]	
+	    else
+	    	@unique_code = SecureRandom.hex(7)
+	    	Refer.create(user_id: @user_id, post_id:params[:id], unique_code:@unique_code) if @user_id && params[:id]	    	
+	    end
 
 	    @post = Post.find(params[:id])
 	    @photos = @post.photos
