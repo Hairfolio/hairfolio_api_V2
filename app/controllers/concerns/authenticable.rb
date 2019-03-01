382e70bac9f0f0a7b18f291f52467b70ad10c5bd
prakash.vadher <prakash.vadher@agileinfoways.com>
2019-03-01 17:50:29 +0530

    remove sessions

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -1,11 +1,12 @@
 module Authenticable
 
   def current_user
-    if session["warden.user.user.key"]
-      @current_user ||= User.find_by(id: session["warden.user.user.key"][0][0])
-    else
-      @current_user ||= request.headers['Authorization'] ? User.find_by(auth_token: request.headers['Authorization']) : nil
-    end
+    # if session["warden.user.user.key"]
+    #   @current_user ||= User.find_by(id: session["warden.user.user.key"][0][0])
+    # else
+    #   @current_user ||= request.headers['Authorization'] ? User.find_by(auth_token: request.headers['Authorization']) : nil
+    # end
+    @current_user ||= request.headers['Authorization'] ? User.find_by(auth_token: request.headers['Authorization']) : nil
   end
 
   def authenticate_with_token!
